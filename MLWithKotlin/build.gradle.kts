e1f1a4eda7e30f0ef4cf83f94ecc8030aa0326b8
Soumic <github@gamegame.anonaddy.me>
2025-03-08 06:48:06 +0600

    :hammer_and_picK: Just practicing in a 2nd language

---
+++ MLWithKotlin/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "ml.from.scratch"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // multik - like numpy
+    implementation("org.jetbrains.kotlinx:multik-core:0.2.0")
+    implementation("org.jetbrains.kotlinx:multik-default:0.2.0") // Native implementation
+    // smile - like scikit-learn
+    implementation("com.github.haifengl:smile-core:2.6.0")
+    // kotlin dl = like tf / keras
+    implementation("org.jetbrains.kotlinx:kotlin-deeplearning-tensorflow:0.6.0-alpha-1")
+    // krangl - like pandas
+    implementation("com.github.holgerbrandl:krangl:0.17")
+
+    // unit test
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
