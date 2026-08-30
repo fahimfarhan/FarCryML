86106748bff16afcace572fa47169c3bf70fc95d
Qazi Fahim Farhan <fahim.farhan@outlook.com>
2026-08-30 15:05:10 +0600

    :hammer: naivebayes.py

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from farcryml!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/_01_naivebayes/naivebayes.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_iris
+
+def main():
+    print("Hello there")
+    pass
+
+if __name__ == "__main__":
+    main()
+    pass
