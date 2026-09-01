e45d5998b78bceef31dcb16491763d346fd81285
Qazi Fahim Farhan <fahim.farhan@outlook.com>
2026-09-01 14:15:31 +0600

    :hammer: Practice decision trees

---
+++ src/_03_decisiontrees/decisiontreeclassifier.py
@@ -0,0 +1,24 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    x, y = load_iris(return_X_y=True, as_frame=False)
+    train_x, test_x, train_y, test_y = train_test_split(x, y, train_size=0.8)
+
+    model = DecisionTreeClassifier()
+    trainedmodel = model.fit(X=train_x, y=train_y)
+
+    predicted_y = trainedmodel.predict(test_x)
+
+    accuracy = accuracy_score(y_true=test_y, y_pred=predicted_y)
+    report = classification_report(y_true=test_y, y_pred=predicted_y)
+    print(f"{accuracy = }")
+    print(report)
+    pass
+
+if __name__ == "__main__":
+    main()
+    pass
---
+++ src/_03_decisiontrees/decisiontreeregression.py
@@ -0,0 +1,27 @@
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.datasets import load_diabetes
+from sklearn.metrics import root_mean_squared_error, mean_squared_error, r2_score
+
+def main():
+    x,y = load_diabetes(return_X_y=True, as_frame=False)
+    train_x, test_x, train_y, test_y = train_test_split(x, y, train_size=0.8)
+
+    model = DecisionTreeRegressor()
+    trainedmodel = model.fit(train_x, train_y)
+
+    predicted_y = trainedmodel.predict(test_x)
+
+    mse = mean_squared_error(y_true=test_y, y_pred=predicted_y)
+    rmse = root_mean_squared_error(y_true=test_y, y_pred=predicted_y)
+    r2 = r2_score(y_true=test_y, y_pred=predicted_y)
+
+    print(f"{mse = }")
+    print(f"{rmse = }")
+    print(f"{r2 = }")
+
+    pass
+
+if __name__ == "__main__":
+    main()
+    pass
